672b501daaf4e25b7455cbbf02f573bc3a58a194
Randy Kreisel <randy@rkreisel.com>
2022-03-05 15:36:10 -0600

    implemented global usings and .net 6 syntax for namespace

--- HotRS.MockLoggingExtensions/MockLoggingExtensions.cs
+++ HotRS.MockLoggingExtensions/MockLoggingExtensions.cs
@@ -1,89 +1,84 @@
-﻿using Microsoft.Extensions.Logging;
-using Moq;
-
-namespace HotRS.MockLoggingExtensions
+﻿namespace HotRS.MockLoggingExtensions;
+/// <summary>
+/// A simple extension to simplify tehr Verification of specific log messages
+/// </summary>
+public static class MockLoggingExtensions
 {
     /// <summary>
-    /// A simple extension to simplify tehr Verification of specific log messages
+    /// The available comparison types
     /// </summary>
-    public static class MockLoggingExtensions
+    public enum ComparisonType
     {
-        /// <summary>
-        /// The available comparison types
-        /// </summary>
-        public enum ComparisonType
-        {
-            Equals,
-            Contains,
-            StartsWith,
-            EndsWith
-        }
+        Equals,
+        Contains,
+        StartsWith,
+        EndsWith
+    }
 
-        public static void VerifyLog<T>(this Mock<ILogger<T>> mockLogger, Func<Times> times)
-        {
-            mockLogger.Verify(x => x.Log(
-                It.IsAny<LogLevel>(),
-                It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => true),
-                It.IsAny<Exception>(),
-                It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), times);
-        }
+    public static void VerifyLog<T>(this Mock<ILogger<T>> mockLogger, Func<Times> times)
+    {
+        mockLogger.Verify(x => x.Log(
+            It.IsAny<LogLevel>(),
+            It.IsAny<EventId>(),
+            It.Is<It.IsAnyType>((v, t) => true),
+            It.IsAny<Exception>(),
+            It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)), times);
+    }
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-        /// <summary>
-        /// An extension method for a generic MOQ Logger to verify if a given string was logged.
-        /// Example:
-        /// mockLogger.VerifyLogContent($"Order number { request.OrderNumber}: Invalid request recieved with errors", Times.Once, MockLoggerExtensions.ComparisonType.StartsWith);
-        /// </summary>
-        /// <typeparam name="T">A generic logger type</typeparam>
-        /// <param name="mockLogger">A logger instance</param>
-        /// <param name="value">The string to verify</param>
-        /// <param name="times">The number of times it should be found</param>
-        /// <param name="compType">The comparison type ([Equals], Contains, StartsWith, or EndsWith)</param>
-        public static void VerifyLogContent<T>(this Mock<ILogger<T>> mockLogger, string value, Func<Times> times, ComparisonType compType = ComparisonType.Equals)
+    /// <summary>
+    /// An extension method for a generic MOQ Logger to verify if a given string was logged.
+    /// Example:
+    /// mockLogger.VerifyLogContent($"Order number { request.OrderNumber}: Invalid request recieved with errors", Times.Once, MockLoggerExtensions.ComparisonType.StartsWith);
+    /// </summary>
+    /// <typeparam name="T">A generic logger type</typeparam>
+    /// <param name="mockLogger">A logger instance</param>
+    /// <param name="value">The string to verify</param>
+    /// <param name="times">The number of times it should be found</param>
+    /// <param name="compType">The comparison type ([Equals], Contains, StartsWith, or EndsWith)</param>
+    public static void VerifyLogContent<T>(this Mock<ILogger<T>> mockLogger, string value, Func<Times> times, ComparisonType compType = ComparisonType.Equals)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        ArgumentNullException.ThrowIfNull(compType);
+        switch (compType)
         {
-            ArgumentNullException.ThrowIfNull(value);
-            ArgumentNullException.ThrowIfNull(compType);
-            switch (compType)
-            {
-                case ComparisonType.Equals:
-                    mockLogger.Verify(x => x.Log(
-                        It.IsAny<LogLevel>(),
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((v, _) => v.ToString().Equals(value)),
-                        It.IsAny<Exception>(),
-                        It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
-                        times);
-                    break;
-                case ComparisonType.Contains:
-                    mockLogger.Verify(x => x.Log(
-                        It.IsAny<LogLevel>(),
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((v, _) => v.ToString().Contains(value)),
-                        It.IsAny<Exception>(),
-                        It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
-                        times);
-                    break;
-                case ComparisonType.StartsWith:
-                    mockLogger.Verify(x => x.Log(
-                        It.IsAny<LogLevel>(),
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((v, _) => v.ToString().StartsWith(value)),
-                        It.IsAny<Exception>(),
-                        It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
-                        times);
-                    break;
-                case ComparisonType.EndsWith:
-                    mockLogger.Verify(x => x.Log(
-                        It.IsAny<LogLevel>(),
-                        It.IsAny<EventId>(),
-                        It.Is<It.IsAnyType>((v, _) => v.ToString().EndsWith(value)),
-                        It.IsAny<Exception>(),
-                        It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
-                        times);
-                    break;
-            }
+            case ComparisonType.Equals:
+                mockLogger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, _) => v.ToString().Equals(value)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
+                    times);
+                break;
+            case ComparisonType.Contains:
+                mockLogger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, _) => v.ToString().Contains(value)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
+                    times);
+                break;
+            case ComparisonType.StartsWith:
+                mockLogger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, _) => v.ToString().StartsWith(value)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
+                    times);
+                break;
+            case ComparisonType.EndsWith:
+                mockLogger.Verify(x => x.Log(
+                    It.IsAny<LogLevel>(),
+                    It.IsAny<EventId>(),
+                    It.Is<It.IsAnyType>((v, _) => v.ToString().EndsWith(value)),
+                    It.IsAny<Exception>(),
+                    It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
+                    times);
+                break;
         }
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
     }
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
 }
