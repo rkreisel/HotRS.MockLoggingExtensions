a414a5d5e9bc25f92f80a043ae1ded90b706c1aa
Randy Kreisel <randy@rkreisel.com>
2022-03-05 14:24:20 -0600

    Updated readme

--- HotRS.MockLoggingExtensions/MockLoggingExtensions.cs
+++ HotRS.MockLoggingExtensions/MockLoggingExtensions.cs
@@ -3,8 +3,14 @@ using Moq;
 
 namespace HotRS.MockLoggingExtensions
 {
+    /// <summary>
+    /// A simple extension to simplify tehr Verification of specific log messages
+    /// </summary>
     public static class MockLoggingExtensions
     {
+        /// <summary>
+        /// The available comparison types
+        /// </summary>
         public enum ComparisonType
         {
             Equals,
@@ -24,6 +30,16 @@ namespace HotRS.MockLoggingExtensions
         }
 
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
+        /// <summary>
+        /// An extension method for a generic MOQ Logger to verify if a given string was logged.
+        /// Example:
+        /// mockLogger.VerifyLogContent($"Order number { request.OrderNumber}: Invalid request recieved with errors", Times.Once, MockLoggerExtensions.ComparisonType.StartsWith);
+        /// </summary>
+        /// <typeparam name="T">A generic logger type</typeparam>
+        /// <param name="mockLogger">A logger instance</param>
+        /// <param name="value">The string to verify</param>
+        /// <param name="times">The number of times it should be found</param>
+        /// <param name="compType">The comparison type ([Equals], Contains, StartsWith, or EndsWith)</param>
         public static void VerifyLogContent<T>(this Mock<ILogger<T>> mockLogger, string value, Func<Times> times, ComparisonType compType = ComparisonType.Equals)
         {
             ArgumentNullException.ThrowIfNull(value);
@@ -34,7 +50,6 @@ namespace HotRS.MockLoggingExtensions
                     mockLogger.Verify(x => x.Log(
                         It.IsAny<LogLevel>(),
                         It.IsAny<EventId>(),
-
                         It.Is<It.IsAnyType>((v, _) => v.ToString().Equals(value)),
                         It.IsAny<Exception>(),
                         It.Is<Func<It.IsAnyType, Exception?, string>>((v, t) => true)),
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # HotRS.MockLoggingExtensions
-Simplified Verify method for unit test using Moq
+This nuget provides an extension to a MOQ Logger<T> object, which simplifies the context to verify the existence that a specified string was logged.
+
+Example:
+
+```c#
+mockLogger.VerifyLogContent("expectedstring", Times.Once, MockLoggerExtensions.ComparisonType.StartsWith);
+```
+
